c847263200b2ed3323ed181e911c534a902f1446
Geofray Paul J <geofraypaul1223@gmail.com>
2024-10-23 15:29:05 +0530

    Update MaskDetect.py

--- MaskDetect.py
+++ MaskDetect.py
@@ -3,11 +3,11 @@ import ultralytics
 import cv2
 from PIL import Image
 import numpy as np
-import time
 
 model = ultralytics.YOLO('best_yet.pt')
 
 st.title("Real-Time Face Mask Detection using YOLOv8")
+
 
 if 'camera_on' not in st.session_state:
     st.session_state.camera_on = False
@@ -17,14 +17,11 @@ if 'cap' not in st.session_state:
 
 video_placeholder = st.empty()
 
-if st.button("Turn On Camera"):
-    st.session_state.camera_on = True
-    st.session_state.cap = cv2.VideoCapture(0)  # Change index if necessary
 
-    # Check if the camera opened successfully
-    if not st.session_state.cap.isOpened():
-        st.warning("Failed to open camera. Please check your device.")
-        st.session_state.camera_on = False  # Reset the state if failed to open
+if st.button("Turn On Camera") and not st.session_state.camera_on:
+    st.session_state.camera_on = True
+    st.session_state.cap = cv2.VideoCapture(0)
+
 
 if st.button("Turn Off Camera") and st.session_state.camera_on:
     st.session_state.camera_on = False
@@ -32,25 +29,34 @@ if st.button("Turn Off Camera") and st.session_state.camera_on:
         st.session_state.cap.release() 
         st.session_state.cap = None
 
+
 if st.session_state.camera_on:
     cap = st.session_state.cap
     if cap is not None:
-        time.sleep(0.5)  # Delay to allow camera to initialize
-
-        while st.session_state.camera_on:
+        while True:
             ret, frame = cap.read()
             if not ret:
                 st.warning("Could not read from camera. Please check your device.")
                 break
 
+           
             results = model(frame)
-            annotated_frame = results[0].plot()  # Annotate the frame with detections
 
-            frame_rgb = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)  # Convert to RGB format
-            img = Image.fromarray(frame_rgb)  # Create an image from the array
+            
+            annotated_frame = results[0].plot()  
 
-            video_placeholder.image(img, use_column_width=True)
+            
+            frame_rgb = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(frame_rgb)
 
-        if st.session_state.cap is not None:
-            st.session_state.cap.release() 
-            st.session_state.cap = None
+            
+            video_placeholder.image(img)
+
+            
+            if not st.session_state.camera_on:
+                break
+
+        
+        st.session_state.cap.release()
+        st.session_state.cap = None
+        cv2.destroyAllWindows()
